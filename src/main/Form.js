12c12e9962b8850aef4de207c3bd0a92c83bc154
“Nicola <nicola.z@gmx.com>
2022-08-10 18:48:51 -0400

    update 4

--- src/main/Form.js
+++ src/main/Form.js
@@ -1,5 +1,7 @@
 import { useContext, useState, useEffect } from "react";
 import styled from "styled-components";
+import { format, compareAsc, parseISO } from "date-fns";
+import { frCA } from "date-fns/locale";
 
 import { WeatherContext } from "../WeatherContext";
 
@@ -37,6 +39,9 @@ const Form = () => {
         fetch(`https://api.weatherbit.io/v2.0/forecast/daily?lat=${cityChosen.lat}&lon=${cityChosen.lon}&key=efcab29fd72a499ebc37f605f0cdb1af&lang=fr`)
             .then(res => res.json())
             .then(data => {
+                const differenceInDay = compareAsc(parseISO(data.data[0].valid_date), parseISO(format(new Date(), "yyyy-MM-dd", { locale: frCA })));
+                if (differenceInDay === 1) { dateIndex-- };
+                if (differenceInDay === - 1) { dateIndex++ };
                 setAllWeather((prev) => [...prev, { date, dateIndex, city: cityChosen.name, weather: data.data[dateIndex] }]);
                 setShowCal(false);
             })
@@ -60,6 +65,13 @@ const Form = () => {
         <Wrapper>
             <form onSubmit={searchCity}>
                 <fieldset>
+                    <legend>
+                        {
+                            allWeather.length > 0
+                                ? <div>Quelle sera votre prochaine destination?</div>
+                                : <div>Quelle sera votre première destination?</div>
+                        }
+                    </legend>
                     <input
                         type="text"
                         value={cityEntered}
@@ -70,58 +82,86 @@ const Form = () => {
             </form>
             {
                 citiesFound.length > 0 &&
-                    <Choices>
+                <Choices>
                         {
-                            citiesFound.map((element, index) => {
-                                return (
-                                    <Choice
-                                        key={index}
-                                        onClick={() => selectCity(element)}
-                                    >
-                                        <div>{element.name}, {element.state}, {element.country}</div>
-                                        <div>lat : {element.lat.toFixed(2)}, lon : {element.lon.toFixed(2)}</div>
-                                    </Choice>
-                                )
-                            })
+                            citiesFound.length > 1
+                                ? <div>Voici les {citiesFound.length} résultats les plus probables.</div>
+                                : <div>Voici le seul résultat :</div>
                         }
-                    </Choices>
+                        
+                    {
+                        citiesFound.map((element, index) => {
+                            return (
+                                <Choice
+                                    key={index}
+                                    onClick={() => selectCity(element)}
+                                >
+                                    <div>{index + 1} – {element.name}, {element.state}, {element.country}</div>
+                                    <div>lat : {element.lat.toFixed(2)}, lon : {element.lon.toFixed(2)}</div>
+                                </Choice>
+                            )
+                        })
+                    }
+                </Choices>
             }
             {
-                cityChosen !== "" &&
-                <>
-                    <div>{cityChosen.name}</div>
-                    {
-                        showCal &&
-                        <DateChoices>
-                        {
-                            allDatesAvailable.map((element, index) => {
-                                return (
-                                    <DateChoice
-                                        key={index}
-                                        onClick={() => selectDate(element, index)}
-                                    >{element.slice(0, 3)} {(element.split(" "))[1]}
-                                    </DateChoice>
-                                )
-                            })
-                        }
-                        </DateChoices>
-                    }
-                    
-                </>
-                
+                cityChosen !== "" && showCal &&
+                <div>Quand serez-vous à {cityChosen.name}?</div>
             }
+            {
+                showCal &&
+                <DateChoices>
+                        
+                    {
+                        allDatesAvailable.map((element, index) => {
+                            return (
+                                <DateChoice
+                                    key={index}
+                                    onClick={() => selectDate(element, index)}
+                                >{element.slice(0, 3)} {(element.split(" "))[1]}
+                                </DateChoice>
+                            )
+                        })
+                    }
+                    </DateChoices>
+            }
+            
         </Wrapper>
     )
 }
 
 const Wrapper = styled.div`
-    padding: 15px;
+    margin: 15px;
     fieldset {
+        border-radius: 5px;
+        display: flex;
+        justify-content: center;
         padding: 5px;
+        legend {
+            padding: 5px;
+        }
+        input {
+            background-color: var(--c-dark);
+            border: 1px solid var(--c-lemon);
+            border-radius: 5px;
+            color: var(--c-light);
+            margin: 10px;
+            padding: 10px;
+        }
+        button {
+            background-color: var(--c-lemon);
+            border: 1px solid var(--c-dark);
+            border-radius: 5px;
+            color: var(--c-dark);
+            cursor: pointer;
+            margin: 10px;
+            padding: 10px;
+        }
     }
 `
 
-const Choices = styled.div``
+const Choices = styled.div`
+`
 
 const Choice = styled.div`
     cursor: pointer;
--- src/main/Summary.js
+++ src/main/Summary.js
@@ -1,10 +1,23 @@
 import { useContext } from "react";
 import styled from "styled-components";
+import {RiCloseCircleLine} from "react-icons/ri"
 
 import { WeatherContext } from "../WeatherContext";
 
 const Summary = () => {
-    const { allWeather } = useContext(WeatherContext);
+    const { allWeather, setAllWeather } = useContext(WeatherContext);
+
+    const removeCity = (city) => {
+        console.log(city);
+        let wholeForecast = [];
+        allWeather.forEach(element => {
+            if (element.weather.sunset_ts !== city.weather.sunset_ts) {
+                wholeForecast.push(element)
+            }
+        })
+        console.log(wholeForecast);
+        setAllWeather(wholeForecast);
+    }
 
     if (allWeather.length > 0) {
         return (
@@ -15,9 +28,53 @@ const Summary = () => {
                         allWeather.map((element, index) => {
                             return (
                                 <OneWeather key={index}>
-                                    <button>Enlever</button>
-                                    <div>{element.city}</div>
-                                    <div>{element.date}</div>
+                                    <BasicInfo>
+                                        <div>
+                                            <div>{element.city}</div>
+                                            <div>{element.date}</div>
+                                        </div>
+                                        <button onClick={() => removeCity(element)}>
+                                            <RiCloseCircleLine size={25} title="Supprimer cette destination" />
+                                        </button>
+                                    </BasicInfo>
+                                    
+                                    <Forecast>
+                                        <div>Température / ressenti °C</div>
+                                        <Temp>
+                                            <TempMin>
+                                                <div>
+                                                    <div>{element.weather.min_temp}</div>
+                                                    <div>MIN</div>
+                                                </div>
+                                                <div>{element.weather.app_min_temp}</div>
+                                            </TempMin>
+                                            <TempMax>
+                                                <div>
+                                                    <div>{element.weather.max_temp}</div>
+                                                    <div>MAX</div>
+                                                </div>
+                                                
+                                                <div>{element.weather.app_max_temp}</div>
+                                            </TempMax>
+                                        </Temp>
+                                        
+                                        <div>{element.weather.weather.description}</div>
+                                        <img src={`https://www.weatherbit.io/static/img/icons/${element.weather.weather.icon}.png`}></img>
+                                        <div>Précip. : {element.weather.pop} %</div>
+                                        {
+                                            element.weather.precip === 0
+                                                ? <div>Aucune précipitation</div>
+                                                : element.weather.precip >= 1
+                                                    ? <div>{parseInt(element.weather.precip)} mm de précip.</div>
+                                                    : <div>{`<`} 1 mm de précip.</div>
+                                        }
+                                        <div>Vents : {element.weather.wind_cdir}, {parseInt(element.weather.wind_spd * 3.6)} km/h.</div>
+                                        {
+                                            element.weather.wind_gust_spd &&
+                                            <div>Rafales à : {parseInt(element.weather.wind_gust_spd * 3.6)} km/h.</div>
+                                        }
+                                    </Forecast>
+                                    
                                 </OneWeather>
                             )
                         })
@@ -34,10 +91,91 @@ const Wrapper = styled.div``
 
 const AllWeather = styled.div`
     display: flex;
+    flex-wrap: wrap;
 `
 
 const OneWeather = styled.div`
+    margin: 15px;
+`
 
+const BasicInfo = styled.div`
+    align-items: center;
+    display: flex;
+    justify-content: space-between;
+    > div {
+        font-weight: bold;
+    }
+    button {
+        align-content: center;
+        background-color: var(--c-light);
+        border: 1px solid var(--c-dark);
+        border-radius: 25%;
+        cursor: pointer;
+        display: flex;
+        height: 100%;
+        &:hover {
+            background-color: var(--c-lemon);
+
+        }
+    }
+`
+
+const Forecast = styled.div`
+    margin-top: 10px;
+`
+
+const Temp = styled.div`
+    color: var(--c-light);
+    display: flex;
+    margin: 5px 0;
+`
+
+const TempMin = styled.div`
+    display: flex;
+    > div:first-child {
+        background-color: var(--c-min-1);
+        border-radius: 5px 0 0 5px;
+        height: fit-content;
+        > div:first-child {
+            font-size: large;
+            margin: 15px 15px 15px 8px;
+        }
+        > div:last-child {
+            margin-bottom: 5px;
+            text-align: center;
+        }
+    }
+    > div:last-child {
+        background-color: var(--c-min-2);
+        border-radius: 0 5px 5px 0;
+        height: fit-content;
+        margin: 20% 0 0 -10%;
+        padding: 10px;
+    }
+`
+
+const TempMax = styled.div`
+    display: flex;
+    > div:first-child {
+        background-color: var(--c-max-1);
+        border-radius: 5px 0 0 5px;
+        height: fit-content;
+        > div:first-child {
+            font-size: large;
+            margin: 15px 15px 15px 8px;
+        }
+        > div:last-child {
+            margin-bottom: 5px;
+            text-align: center;
+        }
+    }
+    > div:last-child {
+        background-color: var(--c-max-2);
+        border-radius: 0 5px 5px 0;
+        height: fit-content;
+        margin: 20% 0 0 -10%;
+        padding: 10px;
+    }
 `
 
 export default Summary;
